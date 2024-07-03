16e70c427a4f083615431f304cde34ab75d5f710
unknown <rasyidsinaga233@gmail.com>
2024-07-03 10:07:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repellat, deserunt delectus beatae amet eveniet non porro exercitationem debitis, illum, quasi adipisci sapiente recusandae. Necessitatibus doloremque impedit numquam tempora ratione ipsum deleniti tempore inventore omnis, totam dolorum alias! Tempore fugiat a, nostrum deserunt dolore reiciendis nihil. Suscipit tempora nam reiciendis?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
