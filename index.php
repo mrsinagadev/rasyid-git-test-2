d7bfce67a3cddad43fb57468124d049b4ad788dd
unknown <rasyidsinaga233@gmail.com>
2024-07-03 10:00:52 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repellat, deserunt delectus beatae amet eveniet non porro exercitationem debitis, illum, quasi adipisci sapiente recusandae. Necessitatibus doloremque impedit numquam tempora ratione ipsum deleniti tempore inventore omnis, totam dolorum alias! Tempore fugiat a, nostrum deserunt dolore reiciendis nihil. Suscipit tempora nam reiciendis?</p>
 </body>
 </html>
